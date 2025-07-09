e28464ccc238c5363486d2b0d337a6857e4cbf9c
zedxter <digaleev@gmail.com>
2025-07-09 09:11:30 +0200

    Initial commit for rust playground

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+fn is_substring(input: &str, term: &str) -> i32 {
+    let term_len = term.len();
+    let input_len = input.len();
+
+    for (i, c) in input.chars().enumerate() {
+        if i + term.len() > input_len {
+            return -1;
+        }
+        if c == term.chars().nth(0).unwrap() && input[i..(i + term_len)].eq(term) {
+            return i as i32;
+        }
+    }
+    -1
+}
+
+fn is_substring_by_char(input: &str, term: &str) -> i32 {
+    let term_len = term.len();
+    let input_len = input.len();
+
+    for (i, _) in input.chars().enumerate() {
+        if i + term_len > input_len {
+            return -1;
+        }
+
+        for (j, c) in term.chars().enumerate() {
+            let i_char = input.chars().nth(i + j).unwrap();
+            if i_char != c {
+                break;
+            }
+            if j == term_len - 1 {
+                return i as i32;
+            }
+        }
+    }
+    -1
+}
+
+fn main() {
+    let input = String::from("Very fancy text!");
+    let term = String::from("fan");
+    let result = is_substring(&input, &term);
+    let result2 = is_substring_by_char(&input, &term);
+
+    println!("The index of substring is {result}");
+    println!("The char by char index of substring is {result2}");
+}
